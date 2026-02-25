de4d3ecc272dc775b3a9918efe9a53089522aca6
jurden <jurden21box@gmail.com>
2026-02-25 22:51:40 +0300

    Add reactions handling

--- main.py
+++ main.py
@@ -20,20 +20,24 @@ with open(json_name, "r", encoding="utf-8") as json_file:
 
 with open(text_name, "w", encoding="UTF-8") as text_file:
 
-    for message in json_data["messages"]:
+    messages = json_data.get("messages", [])
+
+    for message in messages:
         date, time = parse_datetime(message["date"])
 
         if message["type"] == "service":
             text_file.write(date + " " + time + " - Service message: " + str(message) + "\n")
             continue
 
-        message_text = ""
         sender = str(message.get("from", "Unknown"))
+
         photo = str(message.get("photo", ""))
         if photo != "":
             text_file.write(date + " " + time + " - " + sender + ": Photo - " + photo + "\n")
 
-        text_entities = message["text_entities"]
+
+        message_text = ""
+        text_entities = message.get("text_entities", [])
 
         for entity in text_entities:
             if entity["type"] == "plain":
@@ -45,19 +49,39 @@ with open(text_name, "w", encoding="UTF-8") as text_file:
             if entity["type"] == "email":
                 message_text += entity["text"]
 
+            if entity["type"] == "mention":
+                message_text += entity["text"]
+
             if entity["type"] == "bold":
                 message_text += "**" + entity["text"] + "**"
 
             if entity["type"] == "blockquote":
                 text_file.write(date + " " + time + " - " + sender + ": >> " + entity["text"] + "\n")
 
-            if entity["type"] != "plain" and entity["type"] != "link" and entity["type"] != "blockquote" and entity["type"] != "email" and entity["type"] != "bold":
-                print("Script warning =", str(entity["type"]))
+            if entity["type"] != "plain" and entity["type"] != "link" and entity["type"] != "blockquote" and entity["type"] != "email" and entity["type"] != "bold" and entity["type"] != "mention":
+                print("Text Entity unhandled ", str(entity["type"]))
                 print("entity = " + str(entity))
                 print("text = " + message_text)
-                text_file.write("Script warning - Please add a handler of entity:\n")
+                text_file.write("Text Entity unhandled:\n")
+                text_file.write("message = " + str(message) + "\n")
                 text_file.write("entity = " + str(entity) + "\n")
                 text_file.write("text = " + message_text + "\n")
 
         if message_text.strip() != "":
             text_file.write(date + " " + time + " - " + sender + ": " + message_text.strip() + "\n")
+
+
+        reactions = message.get("reactions", [])
+
+        for reaction in reactions:
+            if reaction["type"] == "emoji":
+                for recent in reaction["recent"]:
+                    text_file.write(date + " " + time + " - " + recent["from"] + ": " + reaction["emoji"] + "\n")
+
+            if reaction["type"] != "emoji":
+                print("Reaction unhandled ", str(reaction["type"]))
+                print("reaction = " + str(reaction))
+                text_file.write("Reaction unhandled:\n")
+                text_file.write("message = " + str(message) + "\n")
+                text_file.write("reaction = " + str(reaction) + "\n")
+
